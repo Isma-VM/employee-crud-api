7bcb84f25d4d7d1deadc7e7e0b6cc32d1a132863
Ismael <ima002vm@gmail.com>
2026-07-31 16:33:56 -0400

    feat: add employee repository (in-memory persistence)

---
+++ Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+using EmployeeCrudApi.Models;
+
+namespace EmployeeCrudApi.Repositories;
+
+public interface IEmployeeRepository
+{
+    IEnumerable<Employee> GetAll();
+    Employee? GetById(int id);
+    Employee Add(Employee employee);
+    bool Update(Employee employee);
+    bool Delete(int id);
+    bool ExistsWithEmail(string email, int? excludeId = null);
+}
